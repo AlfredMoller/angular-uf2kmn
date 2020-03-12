f3707bd4043342affc0ccf7840ad496f4d478bea
AlfredMoller <almoller622@gmail.com>
2020-03-11 22:05:23 -0300

    2 files added, 1 deleted

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,14 @@
+export const environment={
+  production:false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCQ4L69y_Chx7fs_FFjYZZa1TNBO461sWg",
+    authDomain: "firestorecrud-96877.firebaseapp.com",
+    databaseURL: "https://firestorecrud-96877.firebaseio.com",
+    projectId: "firestorecrud-96877",
+    storageBucket: "firestorecrud-96877.appspot.com",
+    messagingSenderId: "658877193892",
+    appId: "1:658877193892:web:8d81ec5ab7f7749b5f9031",
+    measurementId: "G-G4J2994Y8K"
+  }
+
+};
